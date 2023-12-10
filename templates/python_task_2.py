cec9bad0e5392f38f564adbfc4f7ad42d42a7a08
Karthik <133976182+KarthikS5@users.noreply.github.com>
2023-12-10 22:39:58 +0530

    python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -13,6 +13,22 @@ def calculate_distance_matrix(df)->pd.DataFrame():
     """
     # Write your logic here
 
+    import pandas as pd
+import networkx as nx
+
+def calculate_distance_matrix(dataset_path):
+    df = pd.read_csv(dataset_path)
+
+    G = nx.from_pandas_edgelist(df, 'id_start', 'id_end', ['distance'])
+
+    distance_matrix = nx.floyd_warshall_numpy(G, weight='distance', nodelist=sorted(G.nodes))
+
+    distance_df = pd.DataFrame(distance_matrix, index=sorted(G.nodes), columns=sorted(G.nodes))
+
+    distance_df.values[[range(len(distance_df))]*2] = 0
+
+    return distance_df
+
     return df
 
 
@@ -27,6 +43,26 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
+
+    def unroll_distance_matrix(df):
+    unrolled_data = []
+
+    for _, row in df.iterrows():
+        id_start = row['id_start']
+        id_end = row['id_end']
+        distance = row['distance']
+
+    
+        for start in df['id_start'].unique():
+            for end in df['id_end'].unique():
+        
+                if start != end:
+                    unrolled_data.append({'id_start': start, 'id_end': end, 'distance': distance})
+
+
+    unrolled_df = pd.DataFrame(unrolled_data)
+
+    return unrolled_df
 
     return df
 
@@ -45,6 +81,20 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
     """
     # Write your logic here
 
+    def find_ids_within_ten_percentage_threshold(df, reference_value):
+    reference_rows = df[df['id_start'] == reference_value]
+    average_distance = reference_rows['distance'].mean()
+    
+    lower_threshold = average_distance - (0.1 * average_distance)
+    upper_threshold = average_distance + (0.1 * average_distance)
+
+    within_threshold = df[(df['distance'] >= lower_threshold) & (df['distance'] <= upper_threshold)]
+
+
+    result_ids = sorted(within_threshold['id_start'].unique())
+    return result_ids
+    
+
     return df
 
 
@@ -59,6 +109,18 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
+    def calculate_toll_rate(df):
+    rate_coefficients = {'moto': 0.8, 'car': 1.2, 'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
+
+    
+    for vehicle_type, rate_coefficient in rate_coefficients.items():
+        df[vehicle_type] = df['distance'] * rate_coefficient
+
+    return df
+
+result_df = calculate_toll_rate(df)
+print(result_df)
+
 
     return df
 
@@ -74,5 +136,46 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+
+    from datetime import time, timedelta
+
+def calculate_time_based_toll_rates(df):
+    time_ranges = [
+        {'start': time(0, 0, 0), 'end': time(10, 0, 0), 'weekday_factor': 0.8, 'weekend_factor': 0.7},
+        {'start': time(10, 0, 0), 'end': time(18, 0, 0), 'weekday_factor': 1.2, 'weekend_factor': 0.7},
+        {'start': time(18, 0, 0), 'end': time(23, 59, 59), 'weekday_factor': 0.8, 'weekend_factor': 0.7}
+    ]
+
+
+    df['start_day'] = df['end_day'] = df['start_time'] = df['end_time'] = None
+
+    
+    for _, row in df.iterrows():
+        id_start = row['id_start']
+        id_end = row['id_end']
+
+        
+        for time_range in time_ranges:
+            
+            df.loc[(df['id_start'] == id_start) & (df['id_end'] == id_end), 'start_day'] = 'Monday'
+            df.loc[(df['id_start'] == id_start) & (df['id_end'] == id_end), 'end_day'] = 'Sunday'
+            df.loc[(df['id_start'] == id_start) & (df['id_end'] == id_end), 'start_time'] = time_range['start']
+            df.loc[(df['id_start'] == id_start) & (df['id_end'] == id_end), 'end_time'] = time_range['end']
+
+            
+            weekday_mask = (df['start_day'] != 'Saturday') & (df['start_day'] != 'Sunday')
+            weekend_mask = ~weekday_mask
+
+            df.loc[(df['id_start'] == id_start) & (df['id_end'] == id_end) & weekday_mask, 'moto':'truck'] *= time_range['weekday_factor']
+            df.loc[(df['id_start'] == id_start) & (df['id_end'] == id_end) & weekend_mask, 'moto':'truck'] *= time_range['weekend_factor']
+
+    
+    df['start_time'] = pd.to_datetime(df['start_time'], format='%H:%M:%S').dt.time
+    df['end_time'] = pd.to_datetime(df['end_time'], format='%H:%M:%S').dt.time
+
+    return df
+
+result_df = calculate_time_based_toll_rates(df)
+print(result_df)
 
     return df
